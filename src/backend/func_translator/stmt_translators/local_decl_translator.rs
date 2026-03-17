1735ee2ee2aa4aab8767d8d2ed7a851778388a86
Hatya-mouse <shuntar30@gmail.com>
2026-03-17 18:07:31 +0900

    Declare variable in translate

--- src/backend/func_translator/stmt_translators/local_decl_translator.rs
+++ src/backend/func_translator/stmt_translators/local_decl_translator.rs
@@ -18,10 +18,10 @@ use crate::{VariableID, backend::func_translator::FuncTranslator};
 
 impl FuncTranslator<'_> {
     pub fn translate_local_var(&mut self, var_id: &VariableID) {
-        // Get the variable
+        // Get the ScopeVar for the id
         let local_var = self.prog_ctx.scope_registry.get_var(var_id).unwrap();
-        // Get the Variable
-        let var = self.variables[var_id];
+        // Declare the variable
+        let var = self.declare_var(*var_id, &local_var.value_type);
 
         // Translate the expression and store the value
         // The variable is a local variable so it should be safe to unwrap the value
@@ -32,8 +32,8 @@ impl FuncTranslator<'_> {
     pub fn translate_local_const(&mut self, var_id: &VariableID) {
         // Get the ScopeVar
         let local_const = self.prog_ctx.scope_registry.get_var(var_id).unwrap();
-        // Get the Variable
-        let var = self.variables[var_id];
+        // Declare the variable
+        let var = self.declare_var(*var_id, &local_const.value_type);
 
         // Translate the expression and store the value
         // The variable is a local constant so it should be safe to unwrap the value
