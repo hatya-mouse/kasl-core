9c0380981d0aa16bc265da86d09d74944dc4a8d6
Hatya-mouse <158461506+Hatya-mouse@users.noreply.github.com>
2026-03-27 11:22:47 +0900

    Copy array and struct by memcopy

--- src/backend/func_translator/utils/copy_value.rs
+++ src/backend/func_translator/utils/copy_value.rs
@@ -15,9 +15,47 @@ impl FuncTranslator<'_> {
         src_offset: i32,
         base_offset: i32,
     ) {
+        // Get the size of the struct from the type registry
         let struct_decl = self.prog_ctx.type_registry.get_struct(struct_id).unwrap();
-        let struct_size = struct_decl.total_size;
+        let struct_size = struct_decl.total_size as u64;
 
+        // Copy the struct to the destination pointer
+        self.copy_with_offset(struct_size, src, dst, src_offset, base_offset);
+    }
+
+    pub fn copy_array(
+        &mut self,
+        array_id: &ArrayID,
+        src: ir::Value,
+        dst: ir::Value,
+        src_offset: i32,
+        base_offset: i32,
+    ) {
+        // Calculate the size of the array in bytes
+        let array_decl = self
+            .prog_ctx
+            .type_registry
+            .get_array_decl(array_id)
+            .unwrap();
+        let item_size = self
+            .prog_ctx
+            .type_registry
+            .get_type_actual_size(array_decl.item_type())
+            .unwrap();
+        let array_size = item_size as u64 * *array_decl.count() as u64;
+
+        // Copy the array to the destination pointer
+        self.copy_with_offset(array_size, src, dst, src_offset, base_offset);
+    }
+
+    fn copy_with_offset(
+        &mut self,
+        total_size: u64,
+        src: ir::Value,
+        dst: ir::Value,
+        src_offset: i32,
+        base_offset: i32,
+    ) {
         // Calculate the src and dst ptr by adding the offset
         let src_ptr = if src_offset != 0 {
             let offset = self
@@ -39,86 +77,16 @@ impl FuncTranslator<'_> {
             dst
         };
 
-        // Copy the struct to the destination pointer
+        // Copy the source to the destination pointer
         self.builder.emit_small_memory_copy(
             self.module.target_config(),
             dst_ptr,
             src_ptr,
-            struct_size as u64,
+            total_size,
             1,
             1,
             true,
             MemFlags::new(),
         );
-    }
-
-    pub fn copy_array(
-        &mut self,
-        array_id: &ArrayID,
-        src: ir::Value,
-        dst: ir::Value,
-        src_offset: i32,
-        base_offset: i32,
-    ) {
-        let array_decl = self
-            .prog_ctx
-            .type_registry
-            .get_array_decl(array_id)
-            .unwrap();
-        // Get the size of the item type from the type registry
-        let item_type = array_decl.item_type();
-        let item_size = self
-            .prog_ctx
-            .type_registry
-            .get_type_actual_size(item_type)
-            .unwrap() as i32;
-
-        let mut offset: i32 = 0;
-        for _ in 0..*array_decl.count() {
-            self.copy_value(item_type, src, dst, src_offset, base_offset, offset);
-            // Increment the offset every iteration
-            offset += item_size;
-        }
-    }
-
-    fn copy_value(
-        &mut self,
-        value_type: &ResolvedType,
-        src: ir::Value,
-        dst: ir::Value,
-        src_offset: i32,
-        base_offset: i32,
-        offset: i32,
-    ) {
-        match value_type {
-            ResolvedType::Primitive(_) => {
-                let ir_type = self.type_converter.convert(value_type);
-                let val =
-                    self.builder
-                        .ins()
-                        .load(ir_type, MemFlags::new(), src, src_offset + offset);
-                self.builder
-                    .ins()
-                    .store(MemFlags::new(), val, dst, base_offset + offset);
-            }
-            ResolvedType::Array(inner_id) => {
-                self.copy_array(
-                    inner_id,
-                    src,
-                    dst,
-                    src_offset + offset,
-                    base_offset + offset,
-                );
-            }
-            ResolvedType::Struct(inner_id) => {
-                self.copy_struct(
-                    inner_id,
-                    src,
-                    dst,
-                    src_offset + offset,
-                    base_offset + offset,
-                );
-            }
-        }
     }
 }
