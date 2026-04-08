11f52603eee282b43467b40ab2c535d5a8573bbb
Hatya-mouse <shuntar30@gmail.com>
2026-04-08 23:51:29 +0900

    Debug print is_prime id

--- src/compiler.rs
+++ src/compiler.rs
@@ -171,7 +171,13 @@ impl KaslCompiler {
         );
         flow_analyzer.analyze_all();
 
-        println!("{:#?}", self.prog_ctx.func_ctx.get_func_map());
+        println!(
+            "is_prime id: {:#?}",
+            self.prog_ctx.func_ctx.get_global_func_id(
+                self.prog_ctx.namespace_registry.get_root_namespace_id(),
+                "is_prime"
+            )
+        );
         println!("{:#?}", comp_data.func_flow_graphs);
 
         // 4. Analyze scope graph
