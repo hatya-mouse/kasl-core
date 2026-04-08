c71bd00c502149b03a670498067d210163d2284f
Hatya-mouse <shuntar30@gmail.com>
2026-04-09 00:42:01 +0900

    Add dst_offset to the dst_ptr in store_array

--- src/lowerer/func_translator/expr_translator/initialization/array_init_store.rs
+++ src/lowerer/func_translator/expr_translator/initialization/array_init_store.rs
@@ -18,7 +18,7 @@ use crate::{
     ast::{Expr, ExprKind, namespace_registry::ArrayID},
     lowerer::func_translator::FuncTranslator,
 };
-use kasl_ir::{InstBuilder, Value};
+use kasl_ir::{InstBuilder, Offset, Value};
 
 impl FuncTranslator<'_> {
     pub(in crate::lowerer::func_translator::expr_translator) fn store_array(
@@ -51,7 +51,8 @@ impl FuncTranslator<'_> {
                 if self.is_zero(value) {
                     // Use memset to fill the target memory with zeros
                     // Get the address of the slot
-                    self.builder.memset(item_size * count, 0, dst_ptr);
+                    let offset_ptr = self.builder.ptr_add(dst_ptr, Offset::Immediate(dst_offset));
+                    self.builder.memset(item_size * count, 0, offset_ptr);
                 } else {
                     for i in 0..*count {
                         let offset = dst_offset + item_size * i;
